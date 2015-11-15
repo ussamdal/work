e5938fd7e36b1415e533a0b669aeefa93700e1c7
jinsu yu <jinsu_yu@tmaxdata.com>
2015-11-15 23:50:42 +0900

    i assign1 finish

--- ocw/1_assign/main.c
+++ ocw/1_assign/main.c
@@ -19,7 +19,8 @@ int main(int argc, char *argv[])
     FILE* fd;
     char input_num[100];
     float num=0;
-
+    int len =0;
+    int i;
     if(argc != 2)
     {
         printf("Please input data file!\n");
@@ -32,26 +33,43 @@ int main(int argc, char *argv[])
         printf("%s : file open fail \n", argv[1]);
         exit(1);
     } 
+    fgets(input_num, sizeof(input_num), fd);
+    len = atoi(input_num);
 
-    while(fgets(input_num, sizeof(input_num), fd))
+
+    for (i=0;i<len; i++)
     {
+        fgets(input_num, sizeof(input_num), fd);
         num = atof(input_num);
-        printf("float input : %f\n", num);
+//        printf("float input : %f\n", num);
         print_bin(num);
-        print_hex(num);
+//        print_hex(num);
     }
-        
+    printf("\n\n");       
     fclose(fd);
     return 0;
 }
 void print_bin(float number)
 {
-    char temp[100];
+    union float_bits t;
+    int i; 
+    t.f = number;
 
-//    temp[0] = (number0x70000000)?'2':'1';
+    if( 0 == (t.bits>>31 & 0x1))
+       printf("1.");
+    else
+        printf("-1.");
 
- //   printf("%s\n", temp);
-
+    for(i = 22; i>=0 ; i--)
+    {
+        if((t.bits >> i) & 0x1)
+            printf("1");
+        else
+            printf("0");
+    }
+    
+    printf(" * 2^%d \n",((t.bits >> 23 ) & 0xff)-127  );
+    
 }
 
 void print_hex(float f)
